198aa88c706608b0afc3fad38755b1098c71927e
jkrowden <jkrowden@gmail.com>
2018-02-06 08:03:17 -0700

    Javascript updated. Adds additional functionality such as the images being changed with the number of guesses remaining, and a click event for each letter button that deletes itself when it is clicked.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,12 @@
 	Make letters appear over underscores
 	Handle a win/loss*/
 
+var underscore = ' ';
 var guesses = -1;
 var wordsetSelection = 'default';
 var word = '@';
-var wordArray = [''];
+var wordArray = [];
+var guessArray = [];
 
 var wordsetDropdown = document.getElementById("wordset-dropdown");
 var wordset1 = document.getElementById("wordset-1");
@@ -28,8 +30,10 @@ var elements = ['HELIUM', 'ARGON', 'POTASSIUM', 'OXYGEN', 'HYDROGEN'];
 var webdev = ['ELEMENT', 'ARRAY', 'JQUERY', 'JAVASCRIPT', 'HTML', 'STRING'];
 var calculus = ['INTEGRAL', 'DERIVATIVE'];
 
+var image = document.getElementById('hangman-image');
 var playGame = false;
 
+// Onload
 window.onload = function () {
 
 	wordset1.addEventListener("click", function () {
@@ -52,31 +56,33 @@ window.onload = function () {
 	});
 }
 
+
 function generateText(array) {
-	word = elements[Math.floor(Math.random() * elements.length)];
+	var hiddenDisplay = '';
+	word = array[Math.floor(Math.random() * elements.length)];
 	wordArray = word.split('');
+
+	underscore = document.createElement('H3');
+
+	console.log(underscore);
 	console.log(word.length);
 
-	for(var i = 0; i < word.length; i++){
-		var underscore = document.createElement('H3');
-		var content = document.createTextNode('_');
-
-		underscore.appendChild(content);
-		underscore.style.display= 'inline-block';
-		underscore.style.fontFamily = 'Dhurjati';
-
-		underscore.id = 'letter-' + i;
-
-		if(i === 0){
-			underscore.style.marginLeft = '0px';
-		}
-		else{
-			underscore.style.marginLeft = '50px';
-		}
-		playLine.appendChild(underscore);
+	for (var i = 0; i < word.length; ++i) {
+		hiddenDisplay += '_ ';
+		guessArray.push('_');
 	}
 
-	console.log(wordArray);
+	var content = document.createTextNode(hiddenDisplay);
+	underscore.appendChild(content);
+	underscore.id = 'hidden-word';
+
+	console.log(hiddenDisplay);
+	playLine.appendChild(underscore);
+	console.log(underscore);
+}
+
+function updateImage() {
+
 }
 
 function playGameButton() {
@@ -92,27 +98,31 @@ function playGameButton() {
 			wordsetDropdown.style.visibility = 'hidden';
 			playLine.removeChild(playButton);
 			generateLetters();
-			generateText();
+			generateText(elements);
 			break;
 		case 'webdev':
 			document.getElementById("status-text").innerHTML = "<h2>Wordset: Web Development</h2>";
 			playButton.style.visibility = 'hidden';
 			wordsetDropdown.style.visibility = 'hidden';
 			generateLetters();
+			generateText(webdev);
 			break;
 		case 'calculus':
 			document.getElementById("status-text").innerHTML = "<h2>Wordset: Calculus</h2>";
 			playButton.style.visibility = 'hidden';
 			wordsetDropdown.style.visibility = 'hidden';
 			generateLetters();
+			generateText(calculus);
 			break;
 		default:
-			alert("No wordset selected!");
+			alert("Please select a wordset to continue!");
 			break;
 	}
 }
 
 function generateLetters() {
+	guesses = 7;
+	setImage();
 	for (var i = 0; i < 26; i++) {
 
 		var letter = document.createElement("BUTTON");
@@ -126,20 +136,101 @@ function generateLetters() {
 		}
 
 		letter.setAttribute('type', "button");
-		letter.id = 'letter-' + i;
+		letter.id = 'letter-' + letters[i];
 		letter.appendChild(content);
+		letter.setAttribute('letter', letters[i]);
 
 		letter.style.position = 'relative';
 		letter.style.marginLeft = '2px';
 
-		letter.addEventListener("click", function(){
-
+		letter.addEventListener("click", function () {
+			findLetters(this.getAttribute('letter'));
 		});
 
 		letterButtons.appendChild(letter);
 	}
 
-	function findLetters(){
+	function findLetters(letter) {
+		console.log(wordArray);
+		console.log(guessArray);
+		console.log(letter);
+
+		if (wordArray.includes(letter)) {
+			for (var i = 0; i < wordArray.length; ++i) {
+				if (wordArray[i] == letter) {
+					guessArray[i] = wordArray[i];
+				}
+			}
+			console.log(guessArray);
+		} else {
+			guesses -= 1;
+		}
+
+		document.getElementById('letter-' + letter).remove();
+		updateGame();
 	}
-	
+
+	function updateGame() {
+		for (var i = 1; i < playLine.childElementCount; i++) {
+			if (playLine.hasChildNodes) {
+				playLine.removeChild(playLine.childNodes[i]);
+			}
+		}
+		playLine.appendChild(underscore);
+
+		var hiddenDisplay2 = '';
+		for (var i = 0; i < word.length; ++i) {
+			if (guessArray[i] != "_") {
+				hiddenDisplay2 += guessArray[i] + ' ';
+			}
+			else {
+				hiddenDisplay2 += '_ ';
+			}
+
+			setImage();
+		}
+
+		var content = document.createTextNode(hiddenDisplay2);
+		underscore.appendChild(content);
+		underscore.removeChild(underscore.childNodes[0]);
+	}
+
+
+	function setImage() {
+		switch (guesses) {
+			case 0:
+				image.src = "./assets/images/hangman-9.png";
+				break;
+			case 1:
+				image.src = "./assets/images/hangman-8.png";
+				break;
+			case 2:
+				image.src = "./assets/images/hangman-7.png";
+				break;
+			case 3:
+				image.src = "./assets/images/hangman-6.png";
+				break;
+			case 4:
+				image.src = "./assets/images/hangman-5.png";
+				break;
+			case 5:
+				image.src = "./assets/images/hangman-4.png";
+				break;
+			case 6:
+				image.src = "./assets/images/hangman-3.png";
+				break;
+			case 7:
+				image.src = "./assets/images/hangman-2.png";
+				break;
+			case 8:
+				image.src = "./assets/images/hangman-1.png";
+				break;
+			case 9:
+				image.src = "./assets/images/hangman-0.png";
+				break;
+			default:
+				break;
+		}
+	}
+
 }
